6da64ebe45bfbe3f63794df0d804d5a0486c6117
Casperliuliuliu <casperliu1118@gmail.com>
2024-01-03 22:17:22 +0800

    adjust mosaic function default para

--- Filters/image_processing.py
+++ Filters/image_processing.py
@@ -121,16 +121,16 @@ if __name__ == "__main__":
     config = get_config()
     filename = config['Test_image']
 
-    height, width = None, 200
+    height, width = None, 256
     img = read_image(filename, height, width)
-    img0 = img.copy()
 
+    img0 = img.copy()
     img1 = adaptiveThreshold_filter(img)
     img2 = canny_filter(img)
     img3 = emboss_filter(img)
-    img4 = mosaic_filter(img, 3)
+    img4 = mosaic_filter(img)
     # img = emboss_filter(img)
 
-    show_image(5, img0, img1, img2, img3)
+    show_image(3, img0, img1, img2, img3,img4)
 
    
