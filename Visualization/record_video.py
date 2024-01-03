c756d06263d79d690d2a2c33a3f06bff5329aa74
Casperliuliuliu <casperliu1118@gmail.com>
2024-01-03 18:36:37 +0800

    add solution, modify filename

--- Visualization/record_video.py
+++ Visualization/record_video.py
@@ -1,43 +1,45 @@
 import cv2
-import time
+from datetime import datetime
 
 """
 Seems like the cv2 Video Writer is not working on MacOS, 
 I spend my afternoon trying different fourcc and changing format with filename, but it just not outputing any video.
 Similiar problem link: https://answers.opencv.org/question/236051/videowriter-not-working-on-macos/
 
+Solved:
+I added the line of code below, and the problem was gone.
+-> cap.set(6, fourcc)
+
 """
 
-def record_video(duration=2):
-    # Initialize video capture
-    cap = cv2.VideoCapture(0)
-    cap.set(3,480)
-    cap.set(4,640)
-    # Check if the webcam is opened correctly
+def record_video(video_source=0, duration=2):
+    cap = cv2.VideoCapture(video_source)
+    
     if not cap.isOpened():
         raise IOError("Cannot open webcam")
 
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc('m','j','p','g')
+
+    height = 480
+    width = 640
+    cap.set(3, height)
+    cap.set(4, width)
     cap.set(6, fourcc)
 
-    out = cv2.VideoWriter('output4.mov', fourcc, 20.0, (640, 480))
+    out = cv2.VideoWriter(f'output{datetime.now()}.mov', fourcc, 20.0, (width, height))
 
-    # start_time = time.time()
-    # while int(time.time() - start_time) < duration:
     while True:
         ret, frame = cap.read()
-        # print(frame.shape)
         if ret:
-            out.write(frame)  # Write frame to video file
-            # Display the frame (optional, can be commented out in production)
+            out.write(frame)
+            
             cv2.imshow('Recording', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         else:
-            break  # If there is no frame, exit
+            break
 
-    # Release the VideoCapture and VideoWriter objects
     cap.release()
     out.release()
     cv2.destroyAllWindows()
