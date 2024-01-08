1520ea439047478ac16d0d641fd574c34c572c9d
casperliuliuliu <casperliu1118@gmail.com>
2024-01-08 12:16:45 +0800

    update both filter by class_list and by num_per_class

--- RunModels/data_loader.py
+++ RunModels/data_loader.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
-# from cprint import pprint
+import random
+# from RunModels.cprint import pprint
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader, Subset, ConcatDataset
@@ -12,6 +13,7 @@ def is_dataset_sorted_by_class(dataset):
             return False
         last_label = label
     return True
+
 def get_num_class_count(dataset):
     class_counts = {}
     for _, label in dataset.samples:
@@ -19,48 +21,39 @@ def get_num_class_count(dataset):
         class_counts[class_name] = class_counts.get(class_name, 0) + 1
     print(class_counts)
 
+def intersection_of_lists(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    intersection = set1.intersection(set2)
+    intersection_list = sorted(list(intersection))
+    return intersection_list
 
-def keep_first_n_per_class(dataset, num_class_count=10):
+def filter_dataset_by_classes_list(original_dataset, classes_list=None):
+    selected_indices = [index for index, (_, label) in enumerate(original_dataset.samples) if label in classes_list]
+    return selected_indices
+
+def filter_dataset_by_num_per_class(original_dataset, num_per_class=-1):
+    if num_per_class < 0:
+        all_indices = list(range(len(original_dataset)))
+        return all_indices
+    
     class_count = {}
     selected_indices = []
-
-    for index, (_, label) in enumerate(dataset.samples):
-        if class_count.get(label, 0) < num_class_count:
+    for index, (_, label) in enumerate(original_dataset.samples):
+        if class_count.get(label, 0) < num_per_class:
             selected_indices.append(index)
             class_count[label] = class_count.get(label, 0) + 1
-    print(selected_indices)
-    # print(class_counts)
-    # get_num_class_count(dataset)
+    return selected_indices
 
-    # class_count = {}
-    # index_list = []
-
-    # for _, class_label in dataset.samples:
-    #     if class_label not in class_count:
-    #         class_count[class_label] = 0
-
-    #     if class_count[class_label] < num_class_count:
-    #         index_list.append(class_label)
-    #         class_count[class_label] += 1
-
-    # # indices_to_keep = [num_class_count - ii -1 for ii in range(num_class_count*2)]
-
-    dataset.samples = [dataset.samples[i] for i in selected_indices]
-    # # merged_dataset.targets = [merged_dataset.targets[i] for i in indices_to_keep]
-
-    # # print(filtered_dataset)
-    class_counts = {}
-    for _, label in dataset.samples:
-        class_name = dataset.classes[label]
-        class_counts[class_name] = class_counts.get(class_name, 0) + 1
-    print(class_counts)
-    # from collections import Counter
-    # print(Counter(dataset.targets))
-    # return filtered_dataset
-    pass
+def filter_dataset_by_classes_list_and_n_per_class(original_dataset, num_per_class, classes_list):
+    num_per_class_indices = filter_dataset_by_num_per_class(original_dataset, num_per_class)
+    by_classes_list_indices = filter_dataset_by_classes_list(original_dataset, classes_list)
+    intersection_list = intersection_of_lists(num_per_class_indices, by_classes_list_indices)
+    sub_dataset = Subset(original_dataset, intersection_list)
+    return sub_dataset
 
 
-def get_datasets(data_dir, data_transforms, train_ratio, val_ratio, random_seed, num_class_count):
+def get_datasets(data_dir, data_transforms, train_ratio, val_ratio, random_seed, num_per_class=-1, classes_list=None):
     if datasets_is_split(data_dir):
         train_path = os.path.join(data_dir, "train")
         val_path = os.path.join(data_dir, "val")
@@ -74,55 +67,56 @@ def get_datasets(data_dir, data_transforms, train_ratio, val_ratio, random_seed,
         test_path = same_dir
 
     train_dataset = datasets.ImageFolder(train_path, transform = data_transforms['train'])
-    print(len(train_dataset))
-    train_dataset = keep_first_n_per_class(train_dataset, num_class_count)
-    print(len(train_dataset))
+    train_dataset = filter_dataset_by_classes_list_and_n_per_class(train_dataset, num_per_class, classes_list)
+
     val_dataset = datasets.ImageFolder(val_path, transform = data_transforms['val'])
+    val_dataset = filter_dataset_by_classes_list_and_n_per_class(val_dataset, -1, classes_list)
+
     test_dataset = datasets.ImageFolder(test_path, transform = data_transforms['test'])
+    test_dataset = filter_dataset_by_classes_list_and_n_per_class(test_dataset, -1, classes_list)
 
-    # for i, class_name in enumerate(train_dataset.classes):
-    #     pprint(f"Class label {i}: {class_name}")
+    for i, class_name in enumerate(train_dataset.dataset.classes):
+        print(f"Class label {i}: {class_name}")
 
-    # if not datasets_is_split(data_dir):
-    #     num_train = len(test_dataset)
-    #     indices = list(range(num_train))
-    #     # pprint("--------- INDEX checking ---------")
-    #     # pprint(f"Original: {indices[:5]}")
-    #     # random.seed(random_seed)
-    #     # random.shuffle(indices)
-    #     # pprint(f"Shuffled: {indices[:5]}")
-    #     # pprint("--------- INDEX shuffled ---------\n")
+    if not datasets_is_split(data_dir):
+        num_train = len(train_dataset.indices)
+        indices = train_dataset.indices
 
-    #     split_train = int(np.floor(train_ratio * num_train))
-    #     split_val = split_train + int(np.floor(val_ratio * (num_train-split_train)))
-    #     train_idx, val_idx, test_idx = indices[0:split_train], indices[split_train:split_val], indices[split_val:]
-    #     train_dataset = Subset(train_dataset, train_idx)
-    #     val_dataset = Subset(val_dataset, val_idx)
-    #     test_dataset = Subset(test_dataset, test_idx)
-        
-    # for ii in range(len(data_transforms.keys())-3):
-    #     aug_dataset = datasets.ImageFolder(train_path, transform = data_transforms[f'aug{ii}'])
-    #     aug_sub = Subset(aug_dataset, train_idx)
-    #     train_dataset = ConcatDataset([train_dataset, aug_sub])
+        random.seed(random_seed)
+        random.shuffle(indices)
 
-    # return {
-    #     'train' : train_dataset,
-    #     'val' : val_dataset,
-    #     'test' : test_dataset,
-    # }
+        split_train = int(np.floor(train_ratio * num_train))
+        split_val = split_train + int(np.floor(val_ratio * (num_train-split_train)))
+        train_idx, val_idx, test_idx = indices[0:split_train], indices[split_train:split_val], indices[split_val:]
+        train_dataset = Subset(train_dataset, train_idx)
+        val_dataset = Subset(val_dataset, val_idx)
+        test_dataset = Subset(test_dataset, test_idx)
 
-def get_dataloaders(data_dir, data_transforms, train_ratio, val_ratio, batch_size, random_seed, max_number_of_data=None, classes_list=None):
+    for ii in range(len(data_transforms.keys())-3):
+        aug_dataset = datasets.ImageFolder(train_path, transform = data_transforms[f'aug{ii}'])
+        aug_sub = Subset(aug_dataset, train_idx)
+        train_dataset = ConcatDataset([train_dataset, aug_sub])
+
+    # print(train_dataset.indices)
+    # print(val_dataset.indices)
+    # print(test_dataset.indices)
+    return {
+        'train' : train_dataset,
+        'val' : val_dataset,
+        'test' : test_dataset,
+    }
+
+def get_dataloaders(data_dir, data_transforms, train_ratio, val_ratio, batch_size, random_seed, num_per_class=-1, classes_list=None):
     
-    datasets = get_datasets(data_dir, data_transforms, train_ratio, val_ratio, random_seed, max_number_of_data)
+    datasets = get_datasets(data_dir, data_transforms, train_ratio, val_ratio, random_seed, num_per_class, classes_list)
     train_loader = DataLoader(datasets['train'], batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(datasets['val'], batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(datasets['test'], batch_size=batch_size, shuffle=True)
         
-    # pprint(f"Total number of samples: {len(train_loader.dataset) + len(val_loader.dataset) + len(test_loader.dataset)} datapoints")
-    # pprint(f"Number of train samples: {len(train_loader)} batches/ {len(train_loader.dataset)} datapoints")
-    # pprint(f"Number of val samples: {len(val_loader)} batches/ {len(val_loader.dataset)} datapoints")
-    # pprint(f"Number of test samples: {len(test_loader)} batches/ {len(test_loader.dataset)} datapoints")
-    # pprint(f"Data Transform: {data_transforms.keys()}\n")
+    print(f"Total number of samples: {len(train_loader.dataset) + len(val_loader.dataset) + len(test_loader.dataset)} datapoints")
+    print(f"Number of train samples: {len(train_loader)} batches/ {len(train_loader.dataset)} datapoints")
+    print(f"Number of val samples: {len(val_loader)} batches/ {len(val_loader.dataset)} datapoints")
+    print(f"Number of test samples: {len(test_loader)} batches/ {len(test_loader.dataset)} datapoints")
     
     dataloaders = {
         "train": train_loader,
@@ -158,4 +152,5 @@ if __name__ == "__main__":
                 transforms.ToTensor(),
             ]),
         }
-    get_datasets(data_dir, data_transforms, 0.6, 0.5, 645, 20)
+    # get_datasets(data_dir, data_transforms, 0.6, 0.5, 645)
+    get_dataloaders(data_dir, data_transforms, 0.6, 0.5, 4, 645, -1, [0,1,2])
